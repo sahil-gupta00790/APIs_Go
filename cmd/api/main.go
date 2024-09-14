a28dd3e5e06830aa7493ad722c1122b84e012171
Sahil Gupta <sahil.gupta.150520@gmail.com>
2024-09-14 14:36:15 +0530

    setup database connection pool , hardcoded the postgres value for now

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 // appn version number
@@ -17,6 +21,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string //dev , staging , produciton , etc.
+	db   struct {
+		dsn string
+	}
 }
 
 // Dependecny Injection
@@ -30,8 +37,22 @@ func main() {
 	//read in flags that are needed to populate our config
 	flag.IntVar(&cfg.port, "port", 4000, "API SERVER PORT")
 	flag.StringVar(&cfg.env, "env", "development", "Environment(development | staging | produciton)")
+
 	flag.Parse()
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDb()
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
+
+	// Open the database connection
+
+	// Ping the database to check if the connection is established
+
+	fmt.Println("Connected to PostgreSQL database!")
+
 	//createing an instance of appn struct
 	app := &application{
 		config: cfg,
@@ -50,6 +71,24 @@ func main() {
 	}
 	//start server
 	logger.Printf("Starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func openDb() (*sql.DB, error) {
+	connStr := "postgres://postgres:WintheW0rld@localhost:5432/apis_go"
+	db, err := sql.Open("pgx", connStr)
+	if err != nil {
+		return nil, err
+	}
+	//create a context with a 5 second tiemout
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+
 }
